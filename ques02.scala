01ae0febee174fcac3ee3137830c17bf78b234b9
nive19s <sothilingamnivethan19@gmail.com>
2024-06-25 00:42:16 +0530

    answered

---
+++ ques02.scala
@@ -0,0 +1,7 @@
+object CelciustoFahrenheit{
+    def main(args:Array[String]):Unit={
+        val celcius = 35
+        val fahrenheit = celcius*1.8 +32
+        println(f"$celcius%d oC is = $fahrenheit%.2foF")
+    }
+}
